8581b64a9342928e79effb44c63d00d6274c7b21
Shivani Gupta <sg3296@columbia.edu>
2016-10-06 17:55:49 -0400

    sql script and some flask session login logic

--- mysql_dao.py
+++ mysql_dao.py
@@ -7,7 +7,7 @@ Created on Wed Oct  5 17:28:00 2016
 
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-DATABASEURI = "mysql+mysqlconnector://aheicklen:mass67@mysql.columbiasurf.dreamhosters.com:3306/columbiasurf" 
+DATABASEURI = "mysql+mysqlconnector://aheicklen:mass67@mysql.columbiasurf.dreamhosters.com:3306/columbiaamgen" 
 engine = create_engine(DATABASEURI)
 
 def createDatabaseConnection():
@@ -19,5 +19,20 @@ def createDatabaseConnection():
         conn = None
     return conn
     
-def createNewUser(conn,name,passwrd):
-    conn.execute('INSERT INTO login_info VALUES (%s, %s)', [name,passwrd])
+def createNewUser(conn,name,passwrd,userid):
+    metadata = MetaData(conn)
+    user_info = Table('userInfo', metadata, autoload=True)
+    s= user_info.select(user_info.c.Username==name)
+    rs = s.execute()
+    if rs.fetchone():
+        return "Error"
+    conn.execute('INSERT INTO userInfo VALUES (%s, %s, %s)', [name,passwrd,userid])
+    
+def getUser(conn,userid):
+    metadata = MetaData(conn)
+    user_info = Table('userInfo', metadata, autoload=True)
+    s= user_info.select(user_info.c.Id==userid)
+    rs = s.execute()
+    a=rs.fetchone()
+    u = User(rs["Username"],rs["Password"],rs["Id"],True)
+    return u
